459177534971afd1b970f8a42b510aecf44a2bd5
Mohnish V S <40720272+MohnishVS@users.noreply.github.com>
2020-01-29 12:44:21 +0530

    Update pascaltriangle.py

--- pascaltriangle.py
+++ pascaltriangle.py
@@ -1,9 +1,8 @@
-def printPascal(n): 
-	for line in range(1, n + 1):
-		C = 1
-		for i in range(1, line + 1):
-			print(C, end = " ")
-			C = int(C * (line - i) / i)
-		print("")
-n=int(input("enter a value to get pascal triangle:"))
-printPascal(n)
+n = int(input('Enter n value: '))
+c = n-1
+c1 = 1
+
+for i in range(n):
+    print((' '*c), ('*'*(c1)))
+    c = c-1
+    c1 = c1+2
